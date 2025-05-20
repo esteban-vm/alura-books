1ac66b62890a5a6eae6b82fcbbed29f1ad535ec7
Esteban V.M. <estebanvm90@outlook.com>
2025-05-20 16:55:02 -0300

    prueba Main

--- src/main/java/com/aluracursos/books/BooksApplication.java
+++ src/main/java/com/aluracursos/books/BooksApplication.java
@@ -1,11 +1,19 @@
 package com.aluracursos.books;
 
+import com.aluracursos.books.main.Main;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BooksApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(BooksApplication.class, args);
-	}
+public class BooksApplication implements CommandLineRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(BooksApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        var main = new Main();
+        main.showMenu();
+    }
 }
