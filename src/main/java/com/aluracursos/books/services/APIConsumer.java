2c13e3a75632ed239e51a3e2f805e006ce1b14c5
Esteban V.M. <estebanvm90@outlook.com>
2025-05-20 16:23:53 -0300

    agrega APIConsumer.java

---
+++ src/main/java/com/aluracursos/books/services/APIConsumer.java
@@ -0,0 +1,30 @@
+package com.aluracursos.books.services;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class APIConsumer {
+    public String getDataFromAPI(String url) {
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+
+        HttpResponse<String> response = null;
+
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            client.close();
+        }
+
+        return response.body();
+    }
+
+}
