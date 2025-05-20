feed16c872875a8b609ce733414b6341e4bce582
Esteban V.M. <estebanvm90@outlook.com>
2025-05-20 16:54:28 -0300

    agrega Book.java

---
+++ src/main/java/com/aluracursos/books/models/Book.java
@@ -0,0 +1,15 @@
+package com.aluracursos.books.models;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record Book(
+        String title,
+        List<Author> authors,
+        List<String> languages,
+        @JsonAlias("download_count") double downloads
+) {
+}
