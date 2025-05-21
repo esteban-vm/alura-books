522cc81bbbb1ee25fa4be31b9bb6fb0c826f1120
Esteban V.M. <estebanvm90@outlook.com>
2025-05-21 08:16:26 -0300

    implementa búsqueda de libro por título

--- src/main/java/com/aluracursos/books/main/Main.java
+++ src/main/java/com/aluracursos/books/main/Main.java
@@ -1,26 +1,67 @@
 package com.aluracursos.books.main;
 
 import com.aluracursos.books.models.APIResponse;
+import com.aluracursos.books.models.Author;
 import com.aluracursos.books.models.Book;
 import com.aluracursos.books.services.APIConsumer;
 import com.aluracursos.books.services.DataConversor;
 
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
 import java.util.Comparator;
+import java.util.Optional;
+import java.util.Scanner;
 
 public class Main {
     private static final String URL = "https://gutendex.com/books/";
     private final APIConsumer consumer = new APIConsumer();
     private final DataConversor conversor = new DataConversor();
+    private final Scanner scanner = new Scanner(System.in);
 
     public void showMenu() {
         var json = consumer.getDataFromAPI(URL);
         var data = conversor.getData(json, APIResponse.class);
 
+        System.out.println("\n\uD83D\uDCDA Los 10 libros más descargados:");
+        
         data.books()
                 .stream()
                 .sorted(Comparator.comparing(Book::downloads).reversed())
                 .limit(10)
-                .map(book -> book.title().toUpperCase())
+                .map(book -> "\uD83D\uDCD6 " + book.title().toUpperCase())
                 .forEach(System.out::println);
+
+        System.out.println("\n\uD83D\uDD0D Ingrese el título del libro que desea buscar:");
+        var bookTitle = scanner.nextLine();
+        var encodedTitle = URLEncoder.encode(bookTitle, Charset.defaultCharset());
+
+        json = consumer.getDataFromAPI(URL + "?search=" + encodedTitle);
+        data = conversor.getData(json, APIResponse.class);
+
+        Optional<Book> searchedBook = data
+                .books()
+                .stream()
+                // .peek(System.out::println)
+                .filter(book -> book.title()
+                        .toUpperCase()
+                        .contains(bookTitle.toUpperCase()))
+                .findFirst();
+
+        if (searchedBook.isPresent()) {
+            System.out.println("\uD83D\uDE3A Libro encontrado");
+
+            Book book = searchedBook.get();
+
+            Optional<String> author = book.authors()
+                    .stream()
+                    .map(Author::name)
+                    .findFirst();
+
+            System.out.println("Título: " + book.title());
+            System.out.println("Autor: " + author.orElse(""));
+            System.out.println("Descargas: " + book.downloads());
+        } else {
+            System.out.println("\uD83D\uDE3F Libro no encontrado");
+        }
     }
 }
